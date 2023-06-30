f1f7a2e3a93f9442c01f1af7c2e1a04a988f3ece
KAUANA  FERREIRA VIANA <kauana.0945@aluno.pr.senac.br>
2023-06-30 16:54:05 -0300

    feito construção tijolos

--- Curriculo/Quebra Tijolo/java.js
+++ Curriculo/Quebra Tijolo/java.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("gameCanvas");
 var desenho = canvas.getContext("2d");
 
 //configurar rebatedor
-var raqueteAltura = 5;
+var raqueteAltura = 7;
 var raqueteLargura = 75;
 var raqueteX = (canvas.width - raqueteLargura) / 2;
 var velocidadeRaquete = 7;
@@ -15,6 +15,25 @@ var bolaX = canvas.width / 2;
 var bolaY = canvas.height - 30;
 var bolaDX = 2;
 var bolaDY = -2;
+
+var tijolosPorLinha = 3;
+var tijolosPorColuna = 6;
+var tijolosLargura = 75;
+var tijolosAltura = 20;
+var tijolosEspacamento = 10;
+var espacamentoSuperiorQuadro = 30;
+var espacamentoEsquerdoQuadro = 30;
+var tijolos = [];
+
+for(var coluna=0; coluna< tijolosPorColuna; coluna++ ){
+    tijolos[coluna] = []
+
+    for(var linha=0; linha < tijolosPorLinha; linha ++){
+
+        tijolos[coluna][linha] = {x:0, y:0, ativo:1 }
+    }
+}
+
  
 var setaDireita = false;
 var setaEsquerda = false;
@@ -30,32 +49,129 @@ function descerDaTecla(tecla){
         }
 
     }
-    function subirDaTecla(){
-
+    function subirDaTecla(tecla){
+        if(tecla.key === "Right" || tecla.key === "ArrowRight" ){
+            setaDireita = false;
+         }else if(tecla.key === "Left" || tecla.key === "ArrowLeft"){
+            setaEsquerda = false;
+         }
     }
 
 
 function desenharRaquete(){
     desenho.beginPath();
     desenho.rect(raqueteX, canvas.height - raqueteAltura, raqueteLargura, raqueteAltura);
-    desenho.fillStyle = "blue";
+    desenho.fillStyle = "black";
+    desenho.fill();
+    desenho.closePath();
+    
+}
+function desenharBola(){
+    desenho.beginPath();
+    desenho.arc(bolaX, bolaY, bolaRadius, 0, Math.PI * 2);
+    desenho.fillStyle = "grey";
     desenho.fill();
     desenho.closePath();
 }
+function desenharTijolos(){
+    for(var coluna=0; coluna < tijolosPorColuna; coluna++){
+        for(var linha=0; linha < tijolosPorLinha; linha ++){
+
+            if(tijolos[coluna][linha].ativo == 1){
+                
+                var tijoloX = (coluna * (tijolosLargura + tijolosEspacamento)+ espacamentoEsquerdoQuadro);
+                var tijoloY = (linha * (tijolosAltura + tijolosEspacamento)+ espacamentoSuperiorQuadro);
+
+                tijolos[coluna][linha].x = tijoloX;
+                tijolos[coluna][linha].y = tijoloY;
+
+                desenho.beginPath();
+                desenho.rect(tijoloX, tijoloY, tijolosLargura, tijolosAltura);
+                desenho.fillStyle = "red";
+                desenho.fill();
+                desenho.closePath();
+
+
+                
+
+            }
+
+        }
+    }
+}
+
+function detectarColisao(){
+    for(var coluna=0; coluna < tijolosPorColuna; coluna++){
+        for(var linha=0; linha < tijolosPorLinha; linha++){
+
+            var tijolo = tijolos[coluna][linha];
+
+            if(tijolo.ativo === 1){
+
+                if(bolaX > tijolo.x
+                    && bolaX < tijolo.x + tijolosLargura
+                    && bolaY > tijolo.y
+                    && bolaY < tijolo.y + tijolosAltura){
+                        bolaDY = -bolaDY;
+                        tijolo.ativo = 0;
+
+                    }
+            }
+
+
+        }
+    }
+}
+
+
 function desenhar(){
     desenho.clearRect(0 ,0, canvas.width, canvas.height);
     desenharRaquete();
+    desenharBola();
+    desenharTijolos();
+    detectarColisao();
 
-    if(setaDireita === true && raqueteX < canvas.width - raqueteLargura){
-        raqueteX = raqueteX + velocidadeRaquete;
+    if (bolaX + bolaDX > canvas.width - bolaRadius || bolaX + bolaDX < bolaRadius){
+        
+        bolaDX = -bolaDX;
 
     }
-    else if(setaEsquerda && raqueteX > 0){
-        raqueteX = raqueteX - velocidadeRaquete;
+
+    if(bolaY + bolaDY < bolaRadius){
+
+        bolaDY = -bolaDY;
+
+    } else if(bolaY + bolaDY > canvas.height - bolaRadius){
+
+        if(bolaX > raqueteX && bolaX < raqueteX + raqueteLargura){
+
+            bolaDY = -bolaDY;
+
+            }else{
+
+                document.location.reload();
+            }
+        }
+
+        if(setaDireita === true && raqueteX < canvas.width - raqueteLargura){
+            raqueteX = raqueteX + velocidadeRaquete;
+    
+        }
+        else if(setaEsquerda === true && raqueteX > 0){
+            raqueteX = raqueteX - velocidadeRaquete;
+        }
+        bolaX = bolaX + bolaDX;
+        bolaY = bolaY + bolaDY;
+
+        requestAnimationFrame(desenhar)
+
     }
 
+ 
+    
 
 
-    requestAnimationFrame(desenhar)
-}
-desenhar();
+
+
+
+desenhar(); 
