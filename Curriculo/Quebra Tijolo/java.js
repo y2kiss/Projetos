5bd30436678344f53634c37ef2f89e02c30a3422
KAUANA  FERREIRA VIANA <kauana.0945@aluno.pr.senac.br>
2023-08-08 15:01:12 -0300

    Criacao do jogo break out

--- Curriculo/Quebra Tijolo/java.js
+++ Curriculo/Quebra Tijolo/java.js
@@ -3,6 +3,7 @@ var canvas = document.getElementById("gameCanvas");
 var desenho = canvas.getContext("2d");
 
 //configurar rebatedor
+
 var raqueteAltura = 7;
 var raqueteLargura = 75;
 var raqueteX = (canvas.width - raqueteLargura) / 2;
@@ -10,29 +11,118 @@ var velocidadeRaquete = 7;
  
 
 //configurar a bola
+
 var bolaRadius = 10;
 var bolaX = canvas.width / 2;
 var bolaY = canvas.height - 30;
 var bolaDX = 2;
-var bolaDY = -2;
+var bolaDY = -3;
 
 var tijolosPorLinha = 3;
-var tijolosPorColuna = 6;
-var tijolosLargura = 75;
+var tijolosPorColuna = 8;
+var tijolosLargura = 60;
 var tijolosAltura = 20;
-var tijolosEspacamento = 10;
+var tijolosEspacamento = 8;
 var espacamentoSuperiorQuadro = 30;
 var espacamentoEsquerdoQuadro = 30;
 var tijolos = [];
 
-for(var coluna=0; coluna< tijolosPorColuna; coluna++ ){
+var totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+var pontuacao = 0;
+
+function facil(){
+    raqueteLargura = 90;
+    tijolosPorLinha = 2;
+    tijolosPorColuna = 5;
+    tijolosLargura = 60;
+    tijolosAltura = 20;
+    bolaRadius = 10;      //tamanho da bola
+    bolaDX = 2;           //velocidade da bola direita/esquerda
+    bolaDY = -2;          //velocidade da bola acima/abaixo
+
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    iniciarTijolos();
+
+}
+
+
+function medio(){
+    raqueteLargura = 75;
+    tijolosPorLinha = 3;
+    tijolosPorColuna = 7;
+    tijolosLargura = 60;
+    tijolosAltura = 20;
+    bolaRadius = 12;      //tamanho da bola
+    bolaDX = 2;           //velocidade da bola direita/esquerda
+    bolaDY = -3;          //velocidade da bola acima/abaixo
+
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    iniciarTijolos();
+
+}
+
+function dificil(){
+    raqueteLargura = 70;
+    tijolosPorLinha = 4;
+    tijolosPorColuna = 10;
+    tijolosLargura = 45;
+    tijolosAltura = 20;
+    bolaRadius = 10;      //tamanho da bola
+    bolaDX = 3;           //velocidade da bola direita/esquerda
+    bolaDY = -3;          //velocidade da bola acima/abaixo
+
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    iniciarTijolos();
+
+}
+
+function impossivel(){
+    raqueteLargura = 70;
+    tijolosPorLinha = 9;
+    tijolosPorColuna = 16;
+    tijolosLargura = 33;
+    tijolosAltura = 20;
+    bolaRadius = 10;      //tamanho da bola
+    bolaDX = 4;           //velocidade da bola direita/esquerda
+    bolaDY = -6;          //velocidade da bola acima/abaixo
+
+    totalPontuacao = tijolosPorLinha * tijolosPorColuna * 10;
+    pontuacao = 0;
+    iniciarTijolos();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function iniciarTijolos(){
+
+
+
+
+for(var coluna = 0; coluna < tijolosPorColuna; coluna++ ){
     tijolos[coluna] = []
 
-    for(var linha=0; linha < tijolosPorLinha; linha ++){
+    for (var linha = 0; linha < tijolosPorLinha; linha ++){
 
-        tijolos[coluna][linha] = {x:0, y:0, ativo:1 }
+        tijolos[coluna][linha] = {x:5, y:5, ativo:1 }
     }
+  
+  }
+
 }
+iniciarTijolos();
 
  
 var setaDireita = false;
@@ -41,7 +131,10 @@ var setaEsquerda = false;
 document.addEventListener("keydown", descerDaTecla);                                                                          
 document.addEventListener("keyup", subirDaTecla);
 
+// TECLA
+
 function descerDaTecla(tecla){
+
     if(tecla.key === "Right" || tecla.key === "ArrowRight" ){
         setaDireita = true;
      }else if(tecla.key === "Left" || tecla.key === "ArrowLeft"){
@@ -58,21 +151,29 @@ function descerDaTecla(tecla){
     }
 
 
+//RAQUETE
+
 function desenharRaquete(){
     desenho.beginPath();
     desenho.rect(raqueteX, canvas.height - raqueteAltura, raqueteLargura, raqueteAltura);
-    desenho.fillStyle = "black";
+    desenho.fillStyle = "rgb(125, 60, 168)";
     desenho.fill();
     desenho.closePath();
     
 }
+
+//BOLA
+
 function desenharBola(){
     desenho.beginPath();
     desenho.arc(bolaX, bolaY, bolaRadius, 0, Math.PI * 2);
-    desenho.fillStyle = "grey";
+    desenho.fillStyle = "black";
     desenho.fill();
     desenho.closePath();
 }
+
+//TIJOLOS
+
 function desenharTijolos(){
     for(var coluna=0; coluna < tijolosPorColuna; coluna++){
         for(var linha=0; linha < tijolosPorLinha; linha ++){
@@ -87,7 +188,7 @@ function desenharTijolos(){
 
                 desenho.beginPath();
                 desenho.rect(tijoloX, tijoloY, tijolosLargura, tijolosAltura);
-                desenho.fillStyle = "red";
+                desenho.fillStyle = "rgb(201, 123, 142)";
                 desenho.fill();
                 desenho.closePath();
 
@@ -95,10 +196,11 @@ function desenharTijolos(){
                 
 
             }
-
         }
     }
 }
+
+//COLISAO
 
 function detectarColisao(){
     for(var coluna=0; coluna < tijolosPorColuna; coluna++){
@@ -109,20 +211,84 @@ function detectarColisao(){
             if(tijolo.ativo === 1){
 
                 if(bolaX > tijolo.x
-                    && bolaX < tijolo.x + tijolosLargura
-                    && bolaY > tijolo.y
-                    && bolaY < tijolo.y + tijolosAltura){
+                    && bolaX - bolaRadius < tijolo.x + tijolosLargura
+                    && bolaY + bolaRadius > tijolo.y
+                    && bolaY - bolaRadius < tijolo.y + tijolosAltura){
                         bolaDY = -bolaDY;
                         tijolo.ativo = 0;
+                        tela = document.getElementById("ponto");
+                        pontuacao = pontuacao + 10;
+                        tela.innerHTML = "Score: "+ pontuacao;
+                        gerarEfeitoSonoro('moeda.mp3');
+                        
 
+                        if(pontuacao === totalPontuacao){
+                            vitoria();
+                        }
                     }
+                }
             }
-
-
         }
     }
+
+
+    contador = 0;
+
+
+//GAMEOVER
+
+function gameover(){
+    var gameover = document.getElementById("gameover");
+    gameover.style.display = "block";
+     
+    if(contador < 1){
+        gerarEfeitoSonoro('gameOver.mp3')
+
+        bolaDX = 0;
+        bolaDY = 0;
+
+    }
+    contador = contador + 1;
+
 }
 
+//REINICIAR
+
+function reiniciar(){
+    document.location.reload();
+
+}
+
+//VITORIA
+
+function vitoria(){
+    var mensagem = document.getElementById("vitoria");
+    mensagem.style.display = "block";
+    gerarEfeitoSonoro('vitoria.mp3')
+
+    bolaDX = 0;
+    bolaDY = 0;
+
+}
+
+//SOM
+
+function gerarEfeitoSonoro(som){
+    var contexto = new (window.AudioContext)();
+    var requisicao = new XMLHttpRequest();
+    requisicao.open('GET',som,true);
+    requisicao.responseType = 'arraybuffer';
+
+    requisicao.onload = function(){ 
+        contexto.decodeAudioData(requisicao.response, function( buffer ){ 
+            var fonte = contexto.createBufferSource();
+            fonte.buffer = buffer;
+            fonte.connect(contexto.destination);
+            fonte.start(0);
+         });
+    }
+    requisicao.send();
+}
 
 function desenhar(){
     desenho.clearRect(0 ,0, canvas.width, canvas.height);
@@ -135,13 +301,15 @@ function desenhar(){
         
         bolaDX = -bolaDX;
 
+        gerarEfeitoSonoro('lateral.mp3');
+
     }
 
     if(bolaY + bolaDY < bolaRadius){
 
         bolaDY = -bolaDY;
 
-    } else if(bolaY + bolaDY > canvas.height - bolaRadius){
+    } else if(bolaY + bolaRadius + bolaDY > canvas.height - bolaRadius){
 
         if(bolaX > raqueteX && bolaX < raqueteX + raqueteLargura){
 
@@ -149,7 +317,7 @@ function desenhar(){
 
             }else{
 
-                document.location.reload();
+                gameover();
             }
         }
 
